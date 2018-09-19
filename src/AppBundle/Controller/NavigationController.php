ccc47150e911f3f97a9543b65671615a24ea581d
VincentVermorel <42928528+VincentVermorel@users.noreply.github.com>
2018-09-19 23:34:13 +0200

    navigation via controller et route Sf

--- src/AppBundle/Controller/NavigationController.php
+++ src/AppBundle/Controller/NavigationController.php
@@ -7,6 +7,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+// Import the BinaryFileResponse
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+
 class NavigationController extends Controller
 {
     /**
@@ -14,8 +17,7 @@ class NavigationController extends Controller
      */
     public function accueilAction()
     {
-        return $this->render('navigation/chantier-du-futur.html.twig',
-        );
+        return $this->render('navigation/chantier-du-futur.html.twig');
     }
 
     /**
@@ -23,9 +25,31 @@ class NavigationController extends Controller
      */
     public function presentationAction()
     {
-        return $this->render('navigation/presentation.html.twig',
-        );
+        return $this->render('navigation/presentation.html.twig');
+    } 
+
+    /**
+     * @Route("/resultats", name="resultats")
+     */
+    public function resultatsAction()
+    {
+        return $this->render('navigation/resultats.html.twig');
     }    
+
+    /**
+     * @Route("/rapport", name="rapport")
+     */
+    public function rapportAction()
+    {
+// i.e Sending a file from the resources folder in /web
+        // in this example, the TextFile.txt needs to exist in the server
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/../web/ressources/';
+        $filename = "BTP 4.0 - le chantier du futur.pdf";
+
+        // This should return the file located in /mySymfonyProject/web/public-resources/TextFile.txt
+        // to being viewed in the Browser
+        return new BinaryFileResponse($publicResourcesFolderPath.$filename);        
+    }
 }
 
 ?>
