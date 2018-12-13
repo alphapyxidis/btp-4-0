2508ad4b190ca0dc27c368767b77e1f591249622
VincentVermorel <42928528+VincentVermorel@users.noreply.github.com>
2018-12-13 10:14:29 +0100

    dossier racine

--- src/AppBundle/Controller/DossierController.php
+++ src/AppBundle/Controller/DossierController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Dossier;
+use AppBundle\Entity\Chantier;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -16,28 +18,6 @@ use Symfony\Component\HttpFoundation\Request;
 class DossierController extends Controller
 {
     /**
-     * Lists all dossier entities.
-     *
-     * @Route("s/", name="dossier_index")
-     * @Method("GET")
-     */
-    public function indexAction(Request $request)
-    {
-        $em    = $this->get('doctrine.orm.entity_manager');
-        $dql   = "SELECT d FROM AppBundle:Dossier d";
-        $query = $em->createQuery($dql);
-    
-        $paginator  = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
-            10/*limit per page*/
-        );
-
-        return $this->render('dossier/index.html.twig', array('pagination' => $pagination));
-    }
-
-    /**
      * Finds and displays a dossier entity.
      *
      * @Route("/{id}", name="dossier_show", defaults={"id" = null}, requirements={"id"="\d+"})
@@ -45,48 +25,48 @@ class DossierController extends Controller
      */
     public function showAction(dossier $dossier)
     {
-        $deleteForm = $this->createDeleteForm($dossier);
+        return $this->render('dossier/show.html.twig', array(
+            'dossier' => $dossier,
+        ));
+    }
+   
+    /**
+     * crée un dossier "virtuel" correspondant à la racine d'un chantier.
+     *
+     * @Route("/{slug}", name="dossier_chantier", defaults={"slug" = null})
+     * @Method("GET")
+     */
+    public function rootAction(Request $request, $slug)
+    {
+
+        $dossier = new Dossier();
+
+        $em = $this->get('doctrine.orm.entity_manager');
+        $repository = $this->getDoctrine()->getRepository(Chantier::class);
+        $chantier = $repository->findOneBySlug($slug); 
+
+        if (empty($chantier)) {
+            throw $this->createNotFoundException('Chantier non trouvé');
+        }
+
+
+        $dossier->setChantier($chantier);
+        $dossier->setNom($chantier->getNom());
+
+        foreach ($chantier->getDossiers() as $sousDossier) {
+            if (is_null($sousDossier->getParent())) {
+                $dossier->addSousDossier($sousDossier);
+            }
+        }
+        foreach ($chantier->getDocuments() as $document) {
+            if (is_null($document->getParent())) {
+                $dossier->addDocument($document);
+            }
+        }
 
         return $this->render('dossier/show.html.twig', array(
             'dossier' => $dossier,
-            'delete_form' => $deleteForm->createView(),
         ));
     }
-    
-    /**
-     * Deletes a dossier entity.
-     *
-     * @Route("/{id}", name="dossier_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, dossier $dossier)
-    {
-        $form = $this->createDeleteForm($dossier);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-       
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($dossier);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('dossier_index');
-    }
-
-    /**
-     * Creates a form to delete a dossier entity.
-     *
-     * @param dossier $dossier The dossier entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(dossier $dossier)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('dossier_delete', array('id' => $dossier->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
 }
